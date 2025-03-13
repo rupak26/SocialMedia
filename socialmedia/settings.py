c789471f9865a824cfd891aa9b3eef5ad6a7488b
Rupak Biswas <rupakbiswas29bd@gmail.com>
2025-03-13 23:20:22 +0600

    Frontend added

--- socialmedia/settings.py
+++ socialmedia/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'contentManagement',
     'interactions',
+    'corsheaders',
     'rest_framework_simplejwt.token_blacklist',  
     'rest_framework.authtoken',
     'django_filters',
@@ -55,8 +56,13 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
+CORS_ALLOW_ALL_ORIGINS = True
 
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173" ,
+]
 ROOT_URLCONF = 'socialmedia.urls'
 
 TEMPLATES = [
