d2044ef8e41330cd720cb5c6a17fc830facc4681
rupak26 <rupakbiswas29bd@gmail.com>
2025-12-08 20:16:34 +0600

    create superuser added

--- UserManagement/models.py
+++ UserManagement/models.py
@@ -2,12 +2,13 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.base_user import AbstractBaseUser
 from .user_manager import userManager
+from django.contrib.auth.models import PermissionsMixin
 
 
-class User(AbstractBaseUser):
+class User(AbstractBaseUser,PermissionsMixin):
     username = models.CharField(
         verbose_name='username',
-        max_length=50,
+        max_length=100,
         blank=True, null=True,unique=True
     )
     email = models.CharField(max_length=200,unique=True)
@@ -15,6 +16,7 @@ class User(AbstractBaseUser):
     new_password = models.CharField(max_length=200)
     otp = models.CharField(max_length=4)
     is_verified = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
     reset_pass = models.CharField(max_length=200)
     objects = userManager()
     USERNAME_FIELD = 'email'
--- UserManagement/user_manager.py
+++ UserManagement/user_manager.py
@@ -11,3 +11,19 @@ class userManager(BaseUserManager):
         user.set_password(password)  # This hashes the password
         user.save(using=self._db)
         return user
+    
+    def create_superuser(self, username, email, password=None):
+        if not email:
+            raise ValueError('Superuser must have an email')
+        if not password:
+            raise ValueError('Superuser must have a password')
+
+        user = self.create_user(username=username, email=email, password=password)
+
+        # Set common admin flags only if they exist on the model
+        for flag in ("is_staff", "is_superuser", "is_admin", "is_active", "is_verified"):
+            if hasattr(user, flag):
+                setattr(user, flag, True)
+
+        user.save(using=self._db)
+        return user 
